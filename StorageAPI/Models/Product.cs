fd9cf3edb30d97088eb98553d6fb94ad7ed6980a
Yaronye <ronya_budak@hotmail.com>
2025-06-25 16:37:31 +0200

    Add project files.

---
+++ StorageAPI/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace StorageAPI.Models
+{
+    public class Product
+    {
+        int Id { get; set; }
+        public string Name { get; set; }
+        int Price { get; set; }
+        string Category;
+        string Shelf { get; set; }
+        int Count {  get; set; }
+        string Description { get; set; }
+
+    }
+}
